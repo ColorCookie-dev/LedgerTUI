96f37686fc36f282395dec67eaf92caa45c47a3c
Color Cookie <colorcookie@protonmail.com>
2023-05-09 13:41:59 +0530

    Added transaction_time to record
    Renamed field names in Record, prefixing with transaction_,
    added time field to Record,
    
    chrono crate added to Utc time and serialization

--- src/ledger/record.rs
+++ src/ledger/record.rs
@@ -1,33 +1,36 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+use chrono::prelude::*;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Record {
-    uuid: Uuid,
-    name: String,
-    amount: i32,
+    transaction_id: Uuid,
+    recipient: String,
+    transaction_amount: i32,
+    transaction_time: DateTime<Utc>,
 }
 
 impl PartialEq for Record {
     fn eq(&self, other: &Self) -> bool {
-        self.uuid == other.uuid
+        self.transaction_id == other.transaction_id
     }
 }
 
 impl Record {
     pub fn name(&self) -> &str {
-        &self.name
+        &self.recipient
     }
 
     pub fn amount(&self) -> i32 {
-        self.amount
+        self.transaction_amount
     }
 
     pub fn new(name: &str, amount: i32) -> Self {
         Self {
-            uuid: Uuid::new_v4(),
-            name: name.to_string(),
-            amount,
+            transaction_id: Uuid::new_v4(),
+            recipient: name.to_string(),
+            transaction_amount: amount,
+            transaction_time: Utc::now(),
         }
     }
 }
